9006bd7df6ce8f84a760fb726aca5a8c6bf75db6
0xanas <anasebrahim514@gmail.com>
2024-06-06 18:41:13 +0300

    Anas changed it

--- scanning/web/url/url_free.py
+++ scanning/web/url/url_free.py
@@ -2,42 +2,9 @@ import subprocess
 import sys
 import requests
 
-def dns_scan(url):
-	print("######################################################################")
-	print("Title: DNS Scanning")
-	print("######################################################################")
-
-	# Check if URL argument is provided
-	if len(sys.argv) != 2:
-	    print("Usage: python script.py <URL>")
-	    sys.exit(1)
-
-	# Write URL to a file
-	with open("host.txt", "w") as file:
-	    file.write(sys.argv[1])
-
-	# Fetch the content of the URL and extract the domain name
-	domain_process = subprocess.Popen(["cat", "host.txt"], stdout=subprocess.PIPE)
-	cut_process = subprocess.Popen(["cut", "-d", "/", "-f", "3"], stdin=domain_process.stdout, stdout=subprocess.PIPE)
-	domain_process.stdout.close()
-	domain, _ = cut_process.communicate()
-
-	# Check if domain extraction was successful
-	if not domain:
-	    print("Failed to extract domain name from URL")
-	    sys.exit(1)
-
-	# Execute dnsenum with the extracted domain name
-	print("Output: ")
-	dnsenum_process = subprocess.run(["dnsenum", "--enum", domain.decode().strip()])
-
-
-	print("######################################################################")
-	print("Description: DNS Scan is used to discover information about DNS servers and their associated domains.")
-
 def param_discovery(url):
 	print("######################################################################")
-	print("Title: Parameter Discovery")
+	print("Parameter Discovery")
 	print("######################################################################")
 	# Get the URL from command line argument
 	url = sys.argv[1]
@@ -51,13 +18,14 @@ def param_discovery(url):
 		checking_params = file.read()
 		if 'Parameters found' in checking_params:
 			result_3 = subprocess.run(f"cat output_2.txt", shell=True, capture_output=True, text=True)
-			print(f"Output: {result_3.stdout}")
+			print(f"{result_3.stdout}")
 		else:
-			print("Output: No parameters exist")
+			print("No parameters exist")
 
 	print("######################################################################")
-	print("Description: Discovering hidden parameters is an important for finding vulnerabilities")
-
+	print("Discovering hidden parameters is an important for finding vulnerabilities")
+	print("######################################################################")
+	print("Mitigate parameter discovery by validating and sanitizing all input parameters and implementing proper access controls.")
 	result_3 = subprocess.run(f"rm output_2.txt file.txt", shell=True, capture_output=True, text=True)
 
 def xss_scan(url):
@@ -67,7 +35,7 @@ def xss_scan(url):
 		sys.exit(1)
 
 	print("######################################################################")
-	print("Title: Scanning XSS Vulnerability")
+	print("Scanning XSS Vulnerability")
 	print("######################################################################")
 	# Get the URL from command line argument
 	url = sys.argv[1]
@@ -79,13 +47,18 @@ def xss_scan(url):
 	with open("unfiltered_tags.txt", "r") as file:
 		unfiltered_tags = file.read()
 		if '<' in unfiltered_tags and '>' in unfiltered_tags and '"' in unfiltered_tags:
-			print(f"Output: This link {url} may be vulnerable to XSS")
+			print(f"This link {url} may be vulnerable to XSS")
 		else:
 			print("Not Vulnerabel to XSS")
+	print("######################################################################")
+	print("XSS (Cross-Site Scripting) is a security vulnerability that allows attackers to inject malicious scripts into web pages viewed by other users. These scripts can steal sensitive information, deface websites, or redirect users to malicious sites.")
+	print("######################################################################")
+	print("To mitigate XSS, always sanitize and encode user input before rendering it in the browser, and use Content Security Policy (CSP) to restrict the sources of executable scripts. Additionally, validate and escape dynamic content in templates to prevent malicious code injection.")
+	result_4 = subprocess.run(f"rm unfiltered_tags.txt", shell=True, capture_output=True, text=True)
 
 def open_redirect_scan(url):
     print("######################################################################")
-    print("Title: Open Redirect Scanning")
+    print("Open Redirect Scanning")
     print("######################################################################")
     response = subprocess.run(f"curl -sI {url}", shell=True, capture_output=True, text=True)
 
@@ -104,21 +77,23 @@ def open_redirect_scan(url):
 
     # Checking for open redirect vulnerability
     if status_code == '302' and 'Location' in headers:
-        print(f"Output: Vulnerable to open redirect: {url} -> {headers['Location']}")
+        print(f"Vulnerable to open redirect: {url} -> {headers['Location']}")
         print("Headers:")
         for key, value in headers.items():
             print(f"{key}: {value}")
     else:
-        print(f"Output: Not vulnerable: {url}")
+        print(f"Not vulnerable: {url}")
 
     print("######################################################################")
-    print("Description: Open redirect is a vulnerability classified as medium severity because it helps attackers in social engineering.")
+    print("Open redirect is a vulnerability classified as medium severity because it helps attackers in social engineering.")
+    print("######################################################################")
+    print("To mitigate open redirect vulnerabilities: Implement server-side URL validation to restrict allowed redirect destinations, Use a whitelist approach to specify trusted redirect URLs and domains.")
 
 def lfi_scan(url):
 	#vuln_link="http://testphp.vulnweb.com/showimage.php?file="
 
 	print("######################################################################")
-	print("Title: Local File Inclusion Scanning")
+	print("Local File Inclusion Scanning")
 	print("######################################################################")
 	# Check if the URL is provided
 	base_url = sys.argv[1]
@@ -126,19 +101,20 @@ def lfi_scan(url):
 	url = f"{base_url}{payload}"
 
 	try:
-	    response = requests.get(url)
+		response = requests.get(url)
 	except requests.exceptions.RequestException as e:
-	    print(f"An error occurred: {e}")
-	    sys.exit(1)
+		print(f"An error occurred: {e}")
+	sys.exit(1)
 
 	if response.status_code == 200:
-	    print("Output: ")
-	    print(response.text)
+		print(response.text)
 	else:
-	    print(f"Output: Failed to retrieve the file. HTTP Status Code: {response.status_code}")
-
+		print(f"Failed to retrieve the file. HTTP Status Code: {response.status_code}")
+	
 	print("######################################################################")
 	print("Description: Local File Inclusion is a critical vulnerability that allow attackers to access sensitive files on the system")
+	print("######################################################################")
+	print("Mitigation of LFI vulnerability involves validating user input and restricting file system access, employing input sanitization, and utilizing whitelists for file inclusion. Additionally, employing proper access controls and avoiding the direct use of user input for file system operations are crucial.")
 
 
 def sqli_scan(url):
@@ -148,7 +124,7 @@ def sqli_scan(url):
 		sys.exit(1)
 
 	print("######################################################################")
-	print("Title: Scanning SQLi Vulnerability")
+	print("Scanning SQLi Vulnerability")
 	print("######################################################################")
 	# Get the URL from command line argument
 	url = sys.argv[1]
@@ -161,13 +137,13 @@ def sqli_scan(url):
 	with open("output_2.txt", "r") as file:
 		checking_payloads = file.read()
 		if 'Type' in checking_payloads or 'Title' in checking_payloads or 'Payload' in checking_payloads:
-			print(f"Output: This link {url} vulnerable to SQLi")
+			print(f"This link {url} vulnerable to SQLi")
 		else:
-			print("Output: Not Vulnerabele to SQLi")
+			print("Not Vulnerabele to SQLi")
 
 	print("######################################################################")
 	print("Description: SQL injection is a critical security vulnerability which allow attackers to retrieve sensitive data from database")
-
+	print("To mitigate SQL injection: Sanitize user inputs by validating and using parameterized queries, Implement proper input validation and encoding to prevent malicious SQL injections.")
 	result_3 = subprocess.run(f"rm output*.txt", shell=True, capture_output=True, text=True)
 
 if __name__ == "__main__":
@@ -176,8 +152,6 @@ if __name__ == "__main__":
         sys.exit(1)
     
     url = sys.argv[1]
-
-    dns_scan(url)
     param_discovery(url)
     xss_scan(url)
     open_redirect_scan(url)
