36741be2d8427fd8935a73b30fe65a7555eddaa7
AreejSalahAli <161175008+AreejSalahAli@users.noreply.github.com>
2024-06-10 13:54:59 +0300

    Update cve.sh

--- scanning/Network2/cve.sh
+++ scanning/Network2/cve.sh
@@ -17,7 +17,7 @@ links=$(echo "$html_content" | grep -oP '<a href="/search\?query=vuln%3ACVE-\d{4
 cves_number=$(echo "$cves" | wc -l)
 
 
-echo "Title: Vulnerabilities $1"
+echo "Title: Vulnerabilities IP: $1"
 echo "######################################################################"
 
 echo "Output: "
@@ -39,4 +39,31 @@ echo -e "This IP $1 is vulnerable with $cves_number CVEs."
 
 echo "######################################################################"
 echo "Description: Scan for vulnerabilities associated with the discovered device(s) using Shodan, enabling identification of potential security weaknesses in the targeted systems and networks."
+echo "######################################################################"
+
+
+echo "Title: Mitigation IP: $1"
+echo "######################################################################"
+
+echo "Output: "
+while IFS= read -r cve; do
+    # Query the NVD database for CVE details and mitigations
+    response=$(curl -s "https://services.nvd.nist.gov/rest/json/cve/1.0/$cve")
+    
+     
+    # Check if the response contains valid JSON
+    if echo "$response" | jq empty 2>/dev/null; then
+        # Extract mitigation using jq
+        mitigation=$(echo "$response" | jq -r '.result.CVE_Items[0].cve.description.description_data[].value')
+        if [ -n "$mitigation" ]; then
+            echo "Mitigation for $cve: $mitigation"
+        else
+            echo "No mitigation found for $cve"
+        fi
+    else
+        echo "Error: Unable to retrieve data for cve"
+    fi
+done <<< "$cves"
+echo "######################################################################"
+echo "Description: Search in NVD Database for mitigation techniques for discovered vulnerabilities"
 echo "######################################################################"
