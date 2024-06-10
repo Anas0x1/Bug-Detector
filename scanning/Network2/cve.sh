d3676ee8ab37d56c1a719a116a04be7e9cb8dcff
AreejSalahAli <161175008+AreejSalahAli@users.noreply.github.com>
2024-06-11 00:23:59 +0300

    Update cve.sh

--- scanning/Network2/cve.sh
+++ scanning/Network2/cve.sh
@@ -17,34 +17,43 @@ cves_number=$(cat cves.txt | wc -l)
 
 echo "Title: Vulnerabilities"
 echo "######################################################################"
-echo "Description: Scan for vulnerabilities associated with the discovered device(s) using Shodan, enabling identification of potential security weaknesses in the targeted systems and networks."
-echo "######################################################################"
-echo "Output: "
-# Iterate over each CVE and its link
-while IFS= read -r cve; do
-    # Fetch the HTML content for the CVE
-    html_content=$(curl -s "https://nvd.nist.gov/vuln/detail/$cve")
-    
-    # Debug: print HTML content to ensure it's being fetched correctly
-    # echo "$html_content" > "$cve.html"
-    
-    # Add Description
-    description=$(echo "$html_content" | grep -oP '(?<=<p data-testid="vuln-description">).*?(?=</p>)' | sed 's/&\#39;/\'"'"'/g')
 
-    echo -e "CVE Number: $cve"
-    echo -e "Description: ${description:-No description found}\n"
-done < cves.txt
+echo "Output: "
+if [ -s "cves.txt" ]; then
+    # Iterate over each CVE and its link
+    while IFS= read -r cve; do
+        # Fetch the HTML content for the CVE
+        html_content=$(curl -s "https://nvd.nist.gov/vuln/detail/$cve")
+    
+        # Debug: print HTML content to ensure it's being fetched correctly
+        # echo "$html_content" > "$cve.html"
+    
+        # Add Description
+        description=$(echo "$html_content" | grep -oP '(?<=<p data-testid="vuln-description">).*?(?=</p>)' | sed 's/&\#39;/\'"'"'/g')
+
+        echo -e "CVE Number: $cve"
+        echo -e "Description: ${description:-No description found}\n"
+    done < cves.txt
+else
+    echo "NO vulnerabilities found for this IP"
+fi
+echo "######################################################################"
+echo "Description: Scan for vulnerabilities associated with the discovered device(s) using Shodan, enabling     identification of potential security weaknesses in the targeted systems and networks."
 echo "######################################################################"
 echo "Mitigation: "
-while IFS= read -r cve; do
+if [ -s "cves.txt" ]; then    
+    while IFS= read -r cve; do
     
-    # Replace the placeholder in the URL with the current CVE
-    mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name=$cve"
-    nist="https://nvd.nist.gov/vuln/detail/$cve"
-    echo "CVE: $cve"
-    echo -e "MITRE Reference: $mitre"
-    echo -e "NIST Reference: $nist\n"
-done < cves.txt
+        # Replace the placeholder in the URL with the current CVE
+        mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name=$cve"
+        nist="https://nvd.nist.gov/vuln/detail/$cve"
+        echo "CVE: $cve"
+        echo -e "MITRE Reference: $mitre"
+        echo -e "NIST Reference: $nist\n"
+    done < cves.txt
+else
+    echo "NO mitigation"
+fi
 echo "######################################################################"
 # Cleanup
 rm cves.txt
